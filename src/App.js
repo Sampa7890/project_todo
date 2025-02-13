eee3c6cecf2ad7b1db369e8736bea30f1db85cba
Sampa7890 <69723129+Sampa7890@users.noreply.github.com>
2025-02-13 12:15:48 +0530

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import TodoForm from './component/TodoForm';
 import TodoWrapper from './component/TodoWrapper';
+
 
 
 function App() {
   return (
     <div className="App">
-      <TodoWrapper/>
+    <TodoWrapper/>
+      <TodoForm/>
     </div>
   );
 }
--- src/component/TodoForm.js
+++ src/component/TodoForm.js
@@ -4,6 +4,10 @@ const TodoForm = () => {
     return (
         <div>
             <h1>Hello I am ToDo Form</h1>
+            <form className='todoForm'>
+                <input type='text' className='todoText' placeholder='Type Your Task'></input>
+                <button type='submit' className='todoSubmit'>Add Task</button>
+            </form>
         </div>
     )
 }
